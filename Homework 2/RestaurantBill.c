1f111faa944ef761b41e073f16317e88cea5773e
dmmargo <dmmargo@cpp.edu>
2018-04-11 14:34:59 -0700

    add homework

---
+++ Homework 2/RestaurantBill.c
@@ -0,0 +1,6 @@
+/*
+Write a program that computes the tax and tip on a restaurant bill for a patron with
+a $88.67 meal charge. The tax should be 6.75 percent of the meal cost. The tip should
+be 20 percent of the total after adding the tax. Display the meal cost, tax amount, tip
+amount, and total bill on the screen. 
+*/
