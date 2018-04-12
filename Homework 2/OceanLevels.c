71b480081cb54b056cb4a578205d8cba7a1cbff7
dmmargo <dmmargo@cpp.edu>
2018-04-11 21:43:40 -0700

    add ocean levels

--- Homework 2/OceanLevels.c
+++ Homework 2/OceanLevels.c
@@ -6,5 +6,19 @@ in 5 years
 • The number of millimeters higher than the current level that the ocean’s level will be
 in 7 years
 • The number of millimeters higher than the current level that the ocean’s level will be
-in 10 years 
+in 10 years
 */
+#include <stdio.h>
+
+int main()
+{
+	double rising = 1.5;
+	double fiveyrs = rising * 5;
+	double sevenyrs = rising * 7;
+	double tenyrs = rising * 10;
+	printf("Number of millimeters higher in 5 years: %.2f millimeters\n", fiveyrs);
+	printf("Number of millimeters higher in 7 years: %.2f millimeters\n", sevenyrs);
+	printf("Number of millimeters higher in 10 years: %.2f millimeters\n", tenyrs);
+
+	return 0;
+}
