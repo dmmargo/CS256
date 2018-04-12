fd7226e0d33ad3fbe911a5d7e86b486e6f11b5a6
dmmargo <dmmargo@cpp.edu>
2018-04-11 22:56:09 -0700

    add pattern

---
+++ Homework 2/PatternDisplays.c
@@ -0,0 +1,27 @@
+//PatternDisplays
+
+#include <stdio.h>
+
+int main()
+{
+	printf("___________________________________________\n");
+	printf("%s%20s\n","PatternA", "PatternB");
+	printf("___________________________________________\n");
+
+	for(int i = 0; i<=10; i++)
+	{
+		for(int j = 0; j<=i; j++)
+		{
+			printf("+");
+
+		}
+		printf("%20s", "");
+		for(int k = 10; k>=i; k--)
+		{
+
+			printf("+");
+		}
+		printf("\n");
+	}
+
+	return 0;
